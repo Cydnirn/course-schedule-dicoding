450826fc7a91ea54ca8b53d0a78b16c907681ade
Seno Aji <cydnirn.dev@gmail.com>
2025-10-19 15:09:45 +0700

    init: Add initial project setup with course scheduling features

---
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDao.kt
@@ -0,0 +1,23 @@
+package com.dicoding.courseschedule.data
+
+import androidx.lifecycle.LiveData
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.sqlite.db.SupportSQLiteQuery
+
+//TODO 2 : Define data access object (DAO) (DONE)
+@Dao
+interface CourseDao {
+
+    fun getNearestSchedule(query: SupportSQLiteQuery): LiveData<Course?>
+
+    fun getAll(query: SupportSQLiteQuery): PagingSource<Int, Course>
+
+    fun getCourse(id: Int): LiveData<Course>
+
+    fun getTodaySchedule(day: Int): List<Course>
+
+    fun insert(course: Course)
+
+    fun delete(course: Course)
+}
---
+++ app/src/main/java/com/dicoding/courseschedule/ui/home/HomeActivity.kt
@@ -0,0 +1,95 @@
+package com.dicoding.courseschedule.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.data.Course
+import com.dicoding.courseschedule.ui.list.ListActivity
+import com.dicoding.courseschedule.ui.setting.SettingsActivity
+import com.dicoding.courseschedule.util.DayName
+import com.dicoding.courseschedule.util.QueryType
+import com.dicoding.courseschedule.util.timeDifference
+import kotlin.jvm.java
+
+//TODO 15 : Write UI test to validate when user tap Add Course (+) Menu, the AddCourseActivity is displayed
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: HomeViewModel
+    private var queryType = QueryType.CURRENT_DAY
+
+    //TODO 5 : Show nearest schedule in CardHomeView and implement menu action (DONE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        supportActionBar?.title = resources.getString(R.string.today_schedule)
+        val factory = HomeViewModelFactory.createFactory(this)
+        viewModel = factory.create(HomeViewModel::class.java)
+
+        viewModel.getNearestSchedule().observe(this) { course ->
+            showNearestSchedule(course)
+        }
+    }
+
+    private fun showNearestSchedule(course: Course?) {
+        checkQueryType(course)
+        course?.apply {
+            val dayName = DayName.getByNumber(day)
+            val time = String.format(getString(R.string.time_format), dayName, startTime, endTime)
+            val remainingTime = timeDifference(day, startTime)
+
+            val cardHome = findViewById<CardHomeView>(R.id.view_home)
+            cardHome.apply {
+                setCourseName(courseName)
+                setTime(time)
+                setRemainingTime(remainingTime)
+                setLecturer(lecturer)
+                setNote(note)
+            }
+        }
+
+        findViewById<TextView>(R.id.tv_empty_home).visibility =
+            if (course == null) View.VISIBLE else View.GONE
+    }
+
+    private fun checkQueryType(course: Course?) {
+        if (course == null) {
+            val newQueryType: QueryType = when (queryType) {
+                QueryType.CURRENT_DAY -> QueryType.NEXT_DAY
+                QueryType.NEXT_DAY -> QueryType.PAST_DAY
+                else -> QueryType.CURRENT_DAY
+            }
+            viewModel.setQueryType(newQueryType)
+            queryType = newQueryType
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.action_list -> {
+                val intent = Intent(this, ListActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.action_add -> {
+                //val intent = Intent(this, AddCourseActivity::class.java)
+                //startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+}
