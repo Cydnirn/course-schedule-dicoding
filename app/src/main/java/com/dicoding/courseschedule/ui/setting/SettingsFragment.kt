1ec7778471312e7c0b6ee08772c4dffa185c6d3e
Seno Aji <cydnirn.dev@gmail.com>
2025-10-19 16:13:37 +0700

    feat: Add theme and notification preferences in SettingsFragment (TODO 10 -11)

--- app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/setting/SettingsFragment.kt
@@ -2,20 +2,56 @@ package com.dicoding.courseschedule.ui.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.notification.DailyReminder
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    private lateinit var DARK_MODE: String
+    private lateinit var NOTIFICATION: String
+    private lateinit var themePreference: ListPreference
+    private lateinit var notificationPreference: SwitchPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         //TODO 10 : Update theme based on value in ListPreference
+        init()
+        themePreference.setOnPreferenceChangeListener { _, newValue ->
+            when (newValue) {
+                "light" -> updateTheme(AppCompatDelegate.MODE_NIGHT_NO)
+                "dark" -> updateTheme(AppCompatDelegate.MODE_NIGHT_YES)
+                "system" -> updateTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                else -> false
+            }
+        }
         //TODO 11 : Schedule and cancel notification in DailyReminder based on SwitchPreference
+        notificationPreference.setOnPreferenceChangeListener { _, newValue ->
+            val isEnabled = newValue as Boolean
+            val dailyReminder = DailyReminder()
+            if (isEnabled) {
+                dailyReminder.setDailyReminder(requireContext())
+            } else {
+                dailyReminder.cancelAlarm(requireContext())
+            }
+            true
+        }
     }
 
     private fun updateTheme(nightMode: Int): Boolean {
         AppCompatDelegate.setDefaultNightMode(nightMode)
         requireActivity().recreate()
         return true
+    }
+
+    private fun init(){
+        DARK_MODE = resources.getString(R.string.pref_key_dark)
+        NOTIFICATION = resources.getString(R.string.pref_key_notify)
+
+        themePreference = findPreference<ListPreference>(DARK_MODE) as ListPreference
+        notificationPreference = findPreference<SwitchPreference>(NOTIFICATION) as SwitchPreference
     }
 }
